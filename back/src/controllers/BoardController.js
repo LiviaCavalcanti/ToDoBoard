976580059af0419559f3da1f4ecb6787456c50f0
docinho <livia.juliao@ccc.ufcg.edu.br>
2019-03-09 00:07:09 -0300

    modificando controller e iniciando front

--- back/src/controllers/BoardController.js
+++ back/src/controllers/BoardController.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
-const Activity = mongoose.model('Activity')
+const ActivityBunch = mongoose.model('ActivityBunch')
 
 model.exports = {
     async index(req, res) {
-        const activities = await Activity.find()
+        const activities = await ActivityBunch.find()
 
         return res.json(activities)
     },
     async store(req, res) {
-        const activity = await Activity.create(req.body)
+        const activity = await ActivityBunch.create(req.body)
 
         return res.json(activity)
     }
