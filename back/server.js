e26588bbcbd3884324cc681a34b18e3bbafc4fbc
docinho <livia.juliao@ccc.ufcg.edu.br>
2019-03-10 22:59:30 -0300

    adicionando cors

--- back/server.js
+++ back/server.js
@@ -1,10 +1,12 @@
 const express = require("express"); // lida com rotas e views
 const mongoose = require("mongoose");
 const requireDir = require("require-dir")
+const cors = require('cors')
 
 // iniciando app
 const app = express();
 app.use(express.json()) // permitir o envio de dados json
+app.use(cors())
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true})
 requireDir('./src/models')
