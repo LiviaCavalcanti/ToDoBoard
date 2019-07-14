ba00a7e51e5223c23f43775d1e74cc7cab5a592d
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-07-14 17:10:19 -0300

    criando pagina inicial

---
+++ front/src/components/Start/index.js
@@ -0,0 +1,36 @@
+import React, { ReactDOM, Component} from 'react'
+import { Link, Route, BrowserRouter, Switch, withRouter} from 'react-router-dom';
+import {ButtonGroup, Button} from 'react-bootstrap'
+
+
+import './index.css'
+import vanGoghFlowers from './teste.jpg';
+import Board from '../Board';
+
+
+class Start extends Component {
+
+  nextPath(path) {
+    this.props.history.push(path);
+  }
+    // Import result is the URL of your image
+    render() {
+      return (
+      
+        <div>
+          <img src={vanGoghFlowers} alt="BackFlowers" className="backPaper"/>
+
+          <div className="d-flex flex-column">
+            <ButtonGroup size="lg">
+              <Button onClick={() => this.nextPath('/schedule') }>Schedule</Button>
+              <Button onClick={() => this.nextPath('/boards') }>Board</Button>
+              <Button onClick={() => this.nextPath('/') }>Start</Button>
+          </ButtonGroup>
+          </div>
+
+        </div>
+      );
+    }  
+}
+  
+export default withRouter(Start)
