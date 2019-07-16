926c73e2bec61a7ee5f3f05d62b873c18a820c2a
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-07-16 15:57:13 -0300

    ajeitando css

--- front/src/components/SideBar/index.js
+++ front/src/components/SideBar/index.js
@@ -11,15 +11,15 @@ class SideBar extends Component {
         return(
             <div className="sidebar">
                 <ul class="nav flex-column">
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <img src={boardImg} alt="boardIcon" className='icons'/>
                         <Link to='./boards' className='navLink'>Board</Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <img src={planImg} alt="planIcon" className='icons'/>
                         <Link to='/schedule' className='navLink'>Schedule</Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <img src={scheduleImg} alt="scheduleIcon" className='icons'/>
                         <Link to='/' className='navLink'>Start</Link>
                     </li>
