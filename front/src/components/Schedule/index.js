66b604a59823525018f3825c6563fca42ad348a2
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-07-16 15:59:18 -0300

    adicionando componente e organizando tela

--- front/src/components/Schedule/index.js
+++ front/src/components/Schedule/index.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
 import ScheduleTable from '../ScheduleTable'
 import SideBar from '../SideBar';
-
+import CollapsedBoard from '../CollapsedBoard'
+import './index.css'
 
 class Schedule extends Component {
     render() {
         return(
-            <div>
-                <SideBar/>
-                <ScheduleTable />
+            <div className='schedule'>
+                <div className='bar'>
+                <SideBar />
+                </div>
+                              
+                <ScheduleTable/>
+                <CollapsedBoard className='collapse'/>
             </div>
         )
     }
