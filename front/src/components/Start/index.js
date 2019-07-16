ee5596e999587274eb49bfb9eac285a193a9dd53
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-07-16 00:03:12 -0300

    ajustando detalhes da tela inicial

--- front/src/components/Start/index.js
+++ front/src/components/Start/index.js
@@ -2,7 +2,6 @@ import React, { Component} from 'react'
 import { withRouter} from 'react-router-dom';
 import {ButtonGroup, Button} from 'react-bootstrap'
 
-
 import './index.css'
 import vanGoghFlowers from './teste.jpg';
 
@@ -12,19 +11,18 @@ class Start extends Component {
   nextPath(path) {
     this.props.history.push(path);
   }
-    // Import result is the URL of your image
     render() {
       return (
       
-        <div>
+        <div className='container'>
           <img src={vanGoghFlowers} alt="BackFlowers" className="backPaper"/>
 
           <div className="d-flex flex-column">
-            <ButtonGroup size="lg">
-              <Button onClick={() => this.nextPath('/schedule') }>Schedule</Button>
-              <Button onClick={() => this.nextPath('/boards') }>Board</Button>
-              <Button onClick={() => this.nextPath('/') }>Start</Button>
-          </ButtonGroup>
+            <ButtonGroup size="lg" className='startButtons'>
+              <Button className='startButton' onClick={() => this.nextPath('/schedule') }>Schedule</Button>
+              <Button className='startButton' onClick={() => this.nextPath('/boards') }>Board</Button>
+              <Button className='startButton' onClick={() => this.nextPath('/') }>Start</Button>
+            </ButtonGroup>
           </div>
 
         </div>
@@ -33,3 +31,5 @@ class Start extends Component {
 }
   
 export default withRouter(Start)
+
+// <div>Icons made by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
