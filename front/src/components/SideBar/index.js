4876424ae6645864d422f02fccddf246807dae26
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-07-16 00:01:15 -0300

    adicionando icones

--- front/src/components/SideBar/index.js
+++ front/src/components/SideBar/index.js
@@ -2,6 +2,9 @@ import React, {Component} from "react"
 import { Link} from 'react-router-dom';
 
 import './index.css'
+import boardImg from './images/board.svg'
+import planImg from './images/planning.svg'
+import scheduleImg from './images/schedule.svg'
 
 class SideBar extends Component {
     render(){
@@ -9,13 +12,16 @@ class SideBar extends Component {
             <div className="sidebar">
                 <ul class="nav flex-column">
                     <li class="nav-item">
-                        <Link to='./boards'>Board</Link>
+                        <img src={boardImg} alt="boardIcon" className='icons'/>
+                        <Link to='./boards' className='navLink'>Board</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to='/schedule'>Schedule</Link>
+                        <img src={planImg} alt="planIcon" className='icons'/>
+                        <Link to='/schedule' className='navLink'>Schedule</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to='/'>Start</Link>
+                        <img src={scheduleImg} alt="scheduleIcon" className='icons'/>
+                        <Link to='/' className='navLink'>Start</Link>
                     </li>
                 </ul>
             </div>
