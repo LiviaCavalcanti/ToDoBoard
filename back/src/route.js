a08512908a66bf0e56c8740186118a72d496c7fa
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-08-04 17:56:19 -0300

    atualizando nome de arquivo e de variavel

--- back/src/route.js
+++ back/src/route.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const routes = express.Router()
 
-const BoardController = require('./controllers/BunchController')
+const BunchController = require('./controllers/BunchController')
 const ActivityController = require('./controllers/ActivityController')
-const scheduledActivityController = require('./controllers/scheduledActivityController')
+const scheduledBunchController = require('./controllers/scheduledBunchController')
 
-routes.get('/bunch', BoardController.index)
-routes.post('/bunch', BoardController.store)
-routes.put('/bunch/:id', BoardController.update)
-routes.get("/bunch/:id", BoardController.findBunch)
+// routes.get("/bunch/:id", BunchController.findBunch)
+routes.get("/bunch/:id", BunchController.show)
+routes.get('/bunch', BunchController.index)
+routes.post('/bunch', BunchController.store)
+routes.put('/bunch/:id', BunchController.update)
 
 routes.put('/activity/:id', ActivityController.update)
 routes.get('/activity/:id', ActivityController.show)
 routes.post('/activity/:bunchid', ActivityController.store)
 routes.delete('/activity/:id', ActivityController.destroy)
 
-routes.put('/scheduledActivity/:id', scheduledActivityController.update)
-routes.get('/scheduledActivity', scheduledActivityController.index)
-routes.post('/scheduledActivity/', scheduledActivityController.store)
-// routes.delete('/scheduledActivity/:id', scheduledActivityController.destroy)
+routes.put('/scheduledBunch/:id', scheduledBunchController.update)
+routes.get('/scheduledBunch', scheduledBunchController.index)
+routes.post('/scheduledBunch/', scheduledBunchController.store)
+// routes.delete('/scheduledBunch/:id', scheduledBunchController.destroy)
 
 module.exports = routes
